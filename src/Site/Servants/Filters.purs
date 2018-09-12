263d38c537b2950426375db4d5882a8be7718529
Joshua Booth <joshua.n.booth@gmail.com>
2018-09-12 00:43:58 -0700

    Rescheduling Nero Fest to tomorrow instead w h o o p s

--- src/Site/Servants/Filters.purs
+++ src/Site/Servants/Filters.purs
@@ -49,11 +49,11 @@ scheduledFilters =
   -- NERO FEST
   ------------
 
-  , ScheduledFilter (ymd 2018 September 12) (ymd 2018 September 24) $
+  , ScheduledFilter (ymd 2018 September 13) (ymd 2018 September 25) $
     namedBonus FilterAvailability "Limited to Event"
     [ "Nero Claudius (Bride)" ]
 
-  , ScheduledFilter (ymd 2018 September 12) (ymd 2018 September 24) $
+  , ScheduledFilter (ymd 2018 September 13) (ymd 2018 September 25) $
     namedBonus FilterAvailability "Rate-Up"
     [ "Nero Claudius (Bride)", "Nero Claudius"
     , "Gaius Julius Caesar", "Romulus", "Boudica"
